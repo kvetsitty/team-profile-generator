c91b67e3ab9699f126a3efeff053a547e8c4a95f
n1zhon1 <kristineetsitty@gmail.com>
2021-10-08 21:50:06 -0700

    added main menu function

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const inquirer = require('inquirer');
 
+function mainMenu() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'Who would you like to add?',
+        choices: ['Add a manager', 'Add an engineer', 'Add an intern', 'Build a team']
+    })
+    .then(function(answers) {
+        const choice = answers.action;
+        if (choice === "Add a manager") {
+            addManager();
+        } else if (choice === "Add an engineer") {
+            addEngineer();
+        } else if (choice === "Add an intern") {
+            addIntern();
+        } else {
+            buildTeam();
+        }
+    })
+}
+
+mainMenu();
